606c4b73821d157dcf0e70cc441e4926093c18d2
ByreddyKundana <byreddykundana@gmail.com>
2021-10-10 20:18:53 +0530

    if_elif_else

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# sets and its methods
-mySet={1,34,53}
-mySet.add(45)
-mySet.add(1)
-mySet.add("1")
-print(mySet)
-mySet.remove(34) 
-print(mySet.pop())
-print(mySet)
-print(len(mySet))
-print(mySet.union({53,11}))
-print(mySet.intersection({11,53}))
+# conditional expresssions
+
+age=int(input("enter your age: "))
+if(age>=18):
+    print("yes")
+else:
+    print("no")
 
 
