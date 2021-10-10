e5f0bf6e686da35408c9143ace75bf17567647ce
ByreddyKundana <byreddykundana@gmail.com>
2021-10-10 13:31:38 +0530

    escape_sequences

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# string functions
-
-mystr="abcdefghijklmnopqrstuvwxyz"
-
-print(len(mystr))
-
-print(mystr.endswith("eer"))
-print(mystr.endswith("xyz"))
-print(mystr.endswith("uvxyz"))
-
-print(mystr.startswith("abcde"))
-
-print(mystr.count('a'))
-print(mystr.count('ab'))
-
-print(mystr.capitalize())
-
-print(mystr.find("cde"))
-
-print(mystr.replace("abc","ABC"))
+# escape sequences
+myname="my name\nis \tpri\"ya"
+print(myname)
