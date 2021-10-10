9e1557a5f680e229c13a656a56a9d69b18bb66ba
ByreddyKundana <byreddykundana@gmail.com>
2021-10-10 15:08:56 +0530

    tuples_intro

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-# list methods
+# tuples
 
-mylist=[1,8,7,2,7,21,15]
-print(mylist)
-a=mylist.sort()  #sorts the list
-print(a)
-print(mylist)
-
-mylist.reverse()   #reverse the list
-print(mylist)
-
-mylist.append(8)
-print(mylist)     #appends at last
-
-mylist.insert(2,9)  #inserts 9 at index2
-print(mylist)
-
-mylist.pop()       #removes an item from the end of the list
-mylist.pop(2)     #removes an item from the index 2
-print(mylist)
-
-mylist.remove(15)
-mylist.remove(7)    #removes first occurence of duplicate items
-print(mylist)
+# mytuple=(3,6,7)
+mytuple=(4,)
+print(mytuple)
+# mytuple[0]=8   #tuples cannot be changed hence throws an error
+print(type(mytuple))
