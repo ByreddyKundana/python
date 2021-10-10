1d74084ba6bb5f0cd9bcea15e1cc497ab0e840a9
ByreddyKundana <byreddykundana@gmail.com>
2021-10-10 14:47:46 +0530

    lists_slicing

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-# escape sequences
-myname="my name\nis \tpri\"ya"
-print(myname)
+# lists and tuples
+
+a=12
+b="this is a string"
+c=False
+
+mylist=[a,b,c,34,56.5]
+print(mylist)
+print(type(mylist))
+
+# list indexing
+
+print(mylist[0])
+
+# list slicing
+print(mylist[0:4])
+print(mylist[0:5])
+print(mylist[0:6])
+print(mylist[0:4:2])
