bb02f82ae13def27f1c36c51e8d28c9aa2e7dad1
ByreddyKundana <byreddykundana@gmail.com>
2021-10-10 11:06:38 +0530

    type

--- var_data_ope.py
+++ var_data_ope.py
@@ -1,41 +1,9 @@
-a=34
-c=56.23
-print(a)
-print("a")
-print(c)
-d=None
-print(d)
-a=75
-b=64
-# arithmetic operators in python
-print("demonstrating arithmetic operators")
-print("a+b= ",a+b)
-print("a-b= ",a-b)
-print("a*b= ",a*b)
-print("a/b= ",a/b)
+a="this is"
+b=34
+c=None
+d=False
 
-# assignment operators in python
-print("demonstrating assignment operators")
-c=5
-d=7
-
-print(c,d)
-c+= 5
-d+=1
-print(c,d)
-
-# comparison operators
-print("demonstrating comparison operators")
-e = 6
-f = 9
-
-print(e==f)
-print(e<f)
-print(e>f)
-print(e!=f)
-
-# logical operators
-print("demonstrating logical operators")
-print(e==f and e>f)
-print(e==f or e<f)
-print(not(e>f))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
