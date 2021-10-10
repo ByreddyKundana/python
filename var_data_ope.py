876176059b74e3ffd71ea4f97f75ca629f70a2a1
ByreddyKundana <byreddykundana@gmail.com>
2021-10-10 11:12:59 +0530

    type_casting

--- var_data_ope.py
+++ var_data_ope.py
@@ -1,9 +1,16 @@
-a="this is"
-b=34
-c=None
-d=False
+# type casting
+a=56
+b="56"
+c=int(b)
+
+print(a)
+print(b)
+print(c)
+
+print(a==b)
+print(a==c)
+print(b==c)
 
 print(type(a))
 print(type(b))
 print(type(c))
-print(type(d))
