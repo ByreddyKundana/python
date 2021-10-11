77403cd9089484ea3883accd31e7d65648eec46f
ByreddyKundana <byreddykundana@gmail.com>
2021-10-11 06:43:27 +0530

    loops

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-# conditional expresssions
+# loops
 
-age=int(input("enter your age: "))
-if(age>=18):
-    print("yes")
-else:
-    print("no")
+a=[1,2,3,4,4,"banana"]
+i=0
+while(i<len(a)):
+    print(a[i])
+    i+=1
 
+for i in range(10,34):
+    print(i)
+
+
+# for i in range(len(a)):
+#     print(a[i])
+#     i+=1
+
+for item in a:
+    print(item)
 
