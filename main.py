b64d787c38b5bf21130a93f44fa369b4ec825dda
ByreddyKundana <byreddykundana@gmail.com>
2021-10-11 06:53:10 +0530

    break_continue

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+#brak and continue
 
-# range function
 
-for i in range(10,34,3):
-    print(i)
+a=[1,2,3,4,5]
+
+for item in a:
+    print(item)
+    if(item==3):
+        break
+
+print("we have finished the loop")
+
+for item in a:
+    
+    if(item==3):
+        continue
+    print(item)
+
+print("we have finished the loop")
+
