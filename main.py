a94ca54b2fea7a646bce8c81f8fc5e4f9aa94f02
ByreddyKundana <byreddykundana@gmail.com>
2021-10-11 07:00:55 +0530

    pass

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-# for loop with else
+# pass
 
-
-a=[1,2,3,4,5]
-
-for item in a:
-    print(item)
-    if(item==3):
-        break
-else:
-    print("we are inside else")
-print("we have finished the loop")
-
-for item in a:
-    
-    if(item==3):
-        continue
-    print(item)
-else:
-    print("we are inside else")
-print("we have finished the loop")
-
+for i in range(0,78):
+    pass
+print("do somethimg)")
