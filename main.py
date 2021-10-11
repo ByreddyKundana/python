7848410f89ece817c3b8dbc2bb8c7cc96be9742f
ByreddyKundana <byreddykundana@gmail.com>
2021-10-11 06:45:32 +0530

    range_function

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-# loops
 
-a=[1,2,3,4,4,"banana"]
-i=0
-while(i<len(a)):
-    print(a[i])
-    i+=1
+# range function
 
-for i in range(10,34):
+for i in range(10,34,3):
     print(i)
-
-
-# for i in range(len(a)):
-#     print(a[i])
-#     i+=1
-
-for item in a:
-    print(item)
-
