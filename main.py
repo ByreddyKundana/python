fc590ed166b5f0d64456db186e910179dad4f9af
ByreddyKundana <byreddykundana@gmail.com>
2021-10-13 06:40:02 +0530

    functions_recursions

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-# pass
+# functions with arguments
 
-for i in range(0,78):
-    pass
-print("do somethimg)")
+def average(a,b,c):
+    return(a+b+c)/3
+
+avg=average(3,6,4)
+print(avg)
+
+# function with default arguments
+
+def greet(name="priya"):
+    return "hello "+name
+# a=greet("sivaa")
+a=greet()
+print(a)
+
+# recursions
+
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    return n*factorial(n-1)
+a=factorial(5)
+print(a)
